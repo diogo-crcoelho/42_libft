25d2b42df57c552ee30b192945fc1f78b19d441f
dcarvalh <dcarvalh@student.42lisboa.com>
2022-10-10 16:48:10 +0100

    memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,30 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcarvalh <dcarvalh@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/08 23:57:09 by dcarvalh          #+#    #+#             */
-/*   Updated: 2022/10/10 15:53:20 by dcarvalh         ###   ########.fr       */
+/*   Created: 2022/10/10 15:37:22 by dcarvalh          #+#    #+#             */
+/*   Updated: 2022/10/10 15:58:05 by dcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stdlib.h>
+void	*ft_memcpy(void *dest, const void * src, size_t n)
+{
+	size_t	i;
 
-// **ctype.h**
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-// **string.h**
-void	ft_bzero(void *str, size_t n);
-void	*ft_memcpy(void *dest, const void * src, size_t n);
-void	*ft_memset(void *str, int c, size_t n);
-size_t	ft_strlen(const char *str);
-
-#endif
+	i = -1;
+	while (++i < n)
+		((unsigned char *) dest)[i] = ((unsigned char *)src)[i]; 
+	return (dest);
+}
