891a1c614e1c74014c3322c449d36c8876987f6d
dcarvalh <dcarvalh@student.42lisboa.com>
2022-10-13 00:53:09 +0100

    calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,12 +6,21 @@
 /*   By: dcarvalh <dcarvalh@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 00:30:53 by dcarvalh          #+#    #+#             */
-/*   Updated: 2022/10/13 00:37:56 by dcarvalh         ###   ########.fr       */
+/*   Updated: 2022/10/13 00:53:03 by dcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+*>>alocates requested memory and returns a pointer to it
+* set alocated memory to 0<<
+* 
+* @param nitems: number of items to allocate
+* @param size:size of items to allocate
+* @retval: pointer to allocated memory
+* NULL if allocation fails
+* */
 void	*ft_calloc(size_t nitems, size_t size)
 {
 	void	*ret;
