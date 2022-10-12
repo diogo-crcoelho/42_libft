fe24b395e45053d7ced18b551414084affd4f16e
dcarvalh <dcarvalh@student.42lisboa.com>
2022-10-13 00:07:23 +0100

    docs

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,12 +6,22 @@
 /*   By: dcarvalh <dcarvalh@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 23:14:55 by dcarvalh          #+#    #+#             */
-/*   Updated: 2022/10/12 23:38:37 by dcarvalh         ###   ########.fr       */
+/*   Updated: 2022/10/13 00:06:53 by dcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+*>>checks for the first instance of c in the first n bytes of str<<
+* @note:c is passed as int but treated as unsigned char
+*
+* @param *str:
+* @param c: char to be searched for (int)casted
+* @param n: number of bytes to search
+* @retval: pointer to the first instance of c,
+* NULL if non existent
+* */
 void	*ft_memchr(const void *str, int c, size_t n)
 {
 	size_t	i;
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,12 +6,20 @@
 /*   By: dcarvalh <dcarvalh@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 15:30:14 by dcarvalh          #+#    #+#             */
-/*   Updated: 2022/10/11 16:03:52 by dcarvalh         ###   ########.fr       */
+/*   Updated: 2022/10/13 00:03:43 by dcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+*>>copies up to n-1 bytes of src to dest
+* always NULL terminating the result<< 
+* @param *dest: string to copy to
+* @param *src: string to copy from
+* @param n:number of bytes to copy + 1
+* @retval: size of the string it tried to create.(size of source)
+* */
 size_t	ft_strlcpy(char *dest, const char *src, size_t n)
 {
 	size_t	i;
